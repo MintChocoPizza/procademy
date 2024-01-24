25a5944479315cf0dd6e8d74f6d70743d5a59999
MintChocoPizza <82192334+MintChocoPizza@users.noreply.github.com>
2024-01-24 13:16:36 +0900

    Update

--- windowsSystem/MyongDongKyoJa/MyongDongKyoJa/MyongDongKyoJa.cpp
+++ windowsSystem/MyongDongKyoJa/MyongDongKyoJa/MyongDongKyoJa.cpp
@@ -15,21 +15,75 @@
 HANDLE hSemaphore;
 DWORD randTimeArr[50];
 
+
+// 세마포어를 사용함
 void TakeMeal(DWORD time)
 {
     WaitForSingleObject(hSemaphore, INFINITE);
     _tprintf_s(_T("Enter Customer %d~ \n"), GetCurrentThreadId());
 
-    _tprintf_s(_T("Customer %d having launch~ \n"), GetCurrentThreadId);
+    _tprintf_s(_T("Customer %d having launch~ \n"), GetCurrentThreadId());
     Sleep(1000 * time);     // 식사 주인 상태를 시뮬레이션하는 함수
 
     ReleaseSemaphore(hSemaphore, 1, NULL);
     _tprintf_s(_T("Out Customer %d~ \n\n"), GetCurrentThreadId());
 }
 
-int main()
+unsigned int WINAPI ThreadProc(LPVOID lpParam)
 {
-    
+    TakeMeal((DWORD)lpParam);
+    return 0;
+}
+
+int _tmain(int argc, TCHAR* argv[])
+{
+    DWORD dwThreadIDs[NUM_OF_CUSTOMER];
+    HANDLE hThreads[NUM_OF_CUSTOMER];
+
+    srand((unsigned)time(NULL));    // random function seed 설정
+
+    // 스레드에 절달할 randdom값 총 50개 생성
+    for (int i = 0; i < NUM_OF_CUSTOMER; i++)
+    {
+        randTimeArr[i] = (DWORD)(((double)rand() / (double)RAND_MAX) * RANGE_MAX + RANGE_MIN );
+    }
+
+    // 세마포어 생성
+    hSemaphore = CreateSemaphore(NULL, TABLE_CNT, TABLE_CNT, NULL);
+
+    if (hSemaphore == NULL)
+    {
+        _tprintf(_T("CreateSemaphore error: %d\n"), GetLastError());
+    }
+
+    // Customer를 의미하는 쓰레드 생성
+    for (int i = 0; i < NUM_OF_CUSTOMER; ++i)
+    {
+        hThreads[i] = (HANDLE)_beginthreadex(NULL, 0, ThreadProc, (void*)randTimeArr[i], CREATE_SUSPENDED, (unsigned*)&dwThreadIDs[i]);
+
+        if (hThreads[i] == NULL)
+        {
+            _tprintf_s(_T("Thread creation fault! \n"));
+            return -1;
+        }
+    }
+
+    for (int i = 0; i < NUM_OF_CUSTOMER; ++i)
+    {
+        ResumeThread(hThreads[i]);
+    }
+
+    WaitForMultipleObjects( NUM_OF_CUSTOMER, hThreads, TRUE, INFINITE );
+
+    _tprintf_s(_T("----- END --------------------- \n"));
+
+    for (int i = 0; i < NUM_OF_CUSTOMER; i++)
+    {
+        CloseHandle(hThreads[i]);
+    }
+
+    CloseHandle(hSemaphore);
+    return 0;
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
