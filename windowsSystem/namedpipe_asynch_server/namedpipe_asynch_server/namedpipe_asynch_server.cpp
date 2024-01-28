7ad879bd94f0374d1db6f559197e8e4ab93dae49
MintChocoPizza <82192334+MintChocoPizza@users.noreply.github.com>
2024-01-28 17:19:06 +0900

    update

--- windowsSystem/namedpipe_asynch_server/namedpipe_asynch_server/namedpipe_asynch_server.cpp
+++ windowsSystem/namedpipe_asynch_server/namedpipe_asynch_server/namedpipe_asynch_server.cpp
@@ -24,9 +24,79 @@ int _tmain(int argc, TCHAR* argv[])
             BUF_SIZE / 2,         // 입력 버퍼 사이즈
             20000, NULL);
 
-        if(hPipe == )
+        if (hPipe == INVALID_HANDLE_VALUE)
+        {
+            _tprintf_s(_T("CreateNamedPipe failed"));
+            return -1;
+        }
+
+        BOOL isSuccess;
+        isSuccess = ConnectNamedPipe(hPipe, NULL) ? TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
+        if (isSuccess)
+            CommToClient(hPipe);
+        else
+            CloseHandle(hPipe);
+    }
+    return 1;
+}
+
+int CommToClient(HANDLE hPipe)
+{
+    TCHAR fileName[MAX_PATH];
+    TCHAR dataBuf[BUF_SIZE];
+    BOOL isSuccess;
+    DWORD fileNameSize;
+
+    isSuccess = ReadFile(
+        hPipe, fileName, MAX_PATH * sizeof(TCHAR),
+        &fileNameSize, NULL);
+
+    if (!isSuccess || fileNameSize == 0)
+    {
+        _tprintf_s(_T("Pipe read message error! \n"));
+        return -1;
     }
 
+    FILE* filePtr = _tfopen(fileName, _T("r"));
+    if (filePtr == NULL)
+    {
+        _tprintf_s(_T("File open fault! \n"));
+        return -1;
+    }
+
+    OVERLAPPED overlappedInst;
+    memset(&overlappedInst, 0, sizeof(overlappedInst));
+    overlappedInst.hEvent = CreateEvent(
+        NULL,       // default security attribute
+        TRUE,       // manual-reset event
+        TRUE,       // initial state = non-signaled
+        NULL        // unnamed event object
+    );
+    DWORD bytesWritten = 0, bytesRead = 0;
+    DWORD bytesWrite = 0, bytesTreansfer = 0;
+
+    while (!feof(filePtr))
+    {
+        bytesRead = fread(dataBuf, 1, BUF_SIZE, filePtr);
+        bytesWrite = bytesRead;
+        isSuccess = WriteFile(hPipe, dataBuf, bytesWrite, &bytesWritten, &overlappedInst);
+        
+        if (!isSuccess && GetLastError() != ERROR_IO_PENDING)
+        {
+            _tprintf_s(_T("Pipe write message error! \n"));
+            break;
+        }
+
+        // ********* 다른 작업을 할 수 있는 기회
+        WaitForSingleObject(overlappedInst.hEvent, INFINITE);
+        GetOverlappedResult(hPipe, &overlappedInst, &bytesTreansfer, FALSE);
+        _tprintf_s(_T("Transferred data size: %u \n"), bytesTreansfer);
+    }
+
+    FlushFileBuffers(hPipe);
+    DisconnectNamedPipe(hPipe);
+    CloseHandle(hPipe);
+    return 1;
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
