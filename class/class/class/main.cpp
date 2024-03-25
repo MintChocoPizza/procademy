930b5adc9e7e70db4584b7571e3be88f671f06ec
MintChocoPizza <82192334+MintChocoPizza@users.noreply.github.com>
2024-03-25 19:22:38 +0900

    update

--- class/class/class/main.cpp
+++ class/class/class/main.cpp
@@ -13,7 +13,7 @@ int main()
 	unsigned char* Buff;
 	unsigned char* temp;
 
-	err = fopen_s(&pFile, "test.txt", "r, ccs=UNICODE");
+	err = fopen_s(&pFile, "test.txt", "rb, ccs=UNICODE");
 	
 	fseek(pFile, 0, SEEK_END);
 	lFileSize = ftell(pFile);
