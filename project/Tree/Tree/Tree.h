56b8d62c08f27299bcb1ef85edc3a3fdeecb1afa
MintChocoPizza <82192334+MintChocoPizza@users.noreply.github.com>
2024-04-12 22:29:08 +0900

    tree_start

--- project/Tree/Tree/Tree.h
+++ project/Tree/Tree/Tree.h
@@ -3,39 +3,73 @@
 #ifndef __TREE_H__
 #define __TREE_H__
 
+#include <stdlib.h>
+#include <malloc.h>
+
 namespace OreoPizza
 {
-	template<class DATA>
 	struct st_NODE
 	{
+		int key;
 		
+		st_NODE* pLeft;
+		st_NODE* pRight;
 	};
 
-	template <class DATA>
 	class CTREE
 	{
 	public:
 		CTREE();
 		~CTREE();
 
-	private:
+		void insert(int num);
 
+
+	private:
+		st_NODE *st_Root;
 	};
 
+	OreoPizza::CTREE::CTREE() : st_Root(nullptr)
+	{
+	}
 
+	OreoPizza::CTREE::~CTREE()
+	{
+	}
 
+	inline void CTREE::insert(int num)
+	{
+		st_NODE* pNewNode = (st_NODE*)malloc(sizeof(st_NODE));
 
+		if (pNewNode == NULL)
+			throw;
+
+		pNewNode->key = num;
+		pNewNode->pLeft = nullptr;
+		pNewNode->pRight = nullptr;
+
+		if (st_Root == nullptr)
+		{
+			st_Root = pNewNode;
+			return;
+		}
+
+		
+
+		st_NODE* pCurNode = st_Root;
+
+		while (pCurNode != nullptr)
+		{
+			if (pCurNode->key < num)
+				pCurNode = pCurNode->pRight;
+			else
+				pCurNode = pCurNode->pLeft;
+
+		}
+	}
+
+}
 
 #endif // !__TREE_H__
 
-	template<class DATA>
-	inline CTREE<DATA>::CTREE()
-	{
 
-	}
-
-	template<class DATA>
-	inline CTREE<DATA>::~CTREE()
-	{
-	}
-}
