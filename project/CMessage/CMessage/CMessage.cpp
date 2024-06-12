c6e87b90b6e640560e845213d183177178b16bdc
MintChocoPizza <82192334+MintChocoPizza@users.noreply.github.com>
2024-06-12 21:26:35 +0900

    직렬화 버퍼

---
+++ project/CMessage/CMessage/CMessage.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <Windows.h>
+
+#include "CMessage.h"
+
+CMessage::CMessage()
+{
+	m_chpBuffer = new char[eBUFFER_DEFAULT];
+	m_iBufferSize = eBUFFER_DEFAULT;
+	m_iDataSize = 0;
+	m_chpRead = m_chpWrite = m_chpBuffer;
+}
+
+CMessage::CMessage(int iBufferSize)
+{
+	m_chpBuffer = new char[iBufferSize];
+	m_iBufferSize = eBUFFER_DEFAULT;
+	m_iDataSize = 0;
+	m_chpRead = m_chpWrite = m_chpBuffer;
+}
+
+CMessage::~CMessage()
+{
+	delete[] m_chpBuffer;
+}
+
+CMessage& CMessage::operator=(CMessage& clSrcPacket)
+{
+	// TODO: 여기에 return 문을 삽입합니다.
+
+}
+
+CMessage& CMessage::operator<<(unsigned char byValue)
+{
+	// TODO: 여기에 return 문을 삽입합니다.
+
+}
+
+CMessage& CMessage::operator<<(char chValue)
+{
+	// TODO: 여기에 return 문을 삽입합니다.
+
+}
