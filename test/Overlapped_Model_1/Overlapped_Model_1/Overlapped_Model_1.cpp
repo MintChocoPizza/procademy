9c33c0514b3b5d6fd0cccf56a821e66227f669fc
MintChocoPizza <82192334+MintChocoPizza@users.noreply.github.com>
2024-07-24 22:21:53 +0900

    d

---
+++ test/Overlapped_Model_1/Overlapped_Model_1/Overlapped_Model_1.cpp
@@ -0,0 +1,116 @@
+﻿// Overlapped_Model_1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#pragma comment (lib, "ws2_32")
+#include <iostream>
+#include <WinSock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SERVERPORT 9000
+#define BUFSIZE 512
+
+// 소켓 정보 저장을 위한 구조체와 변수
+struct SOCKETINFO
+{
+	WSAOVERLAPPED overlapped;
+	SOCKET sock;
+	char buf[BUFSIZE + 1];
+	int recvbytes;
+	int sendbbytes;
+	WSABUF wsabuf;
+};
+
+SOCKET client_sock;
+HANDLE hReadEvent, hWriteEvent;
+
+// 비동기 입출력 시작과 처리 함수 
+DWORD __stdcall WorkerThread(void* arg);
+void CALLBACK CompletionRoutine(DWORD dwError, DWORD cbTransferred, LPWSAOVERLAPPED lpOverlapped, DWORD dwFlags);
+
+
+int main()
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
+
+	// socket()
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET)
+		printf("socket()");
+
+	// bind()
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR)
+		printf("bind()");
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR)
+		printf("listen()");
+
+	// 이벤트 객체 생성
+	hReadEvent = CreateEvent(NULL, FALSE, TRUE, NULL);
+	if (hReadEvent == NULL) return 1;
+	hWriteEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+	if (hWriteEvent == NULL) return 1;
+
+	// 스레드 생성 
+	HANDLE hThread = CreateThread(NULL, 0, WorkerThread, NULL, 0, NULL);
+	if (hThread == NULL) return 1;
+	CloseHandle(hThread);
+
+	while (1)
+	{
+		WaitForSingleObject(hReadEvent, INFINITE);
+		// accept()
+		client_sock = accept(listen_sock, NULL, NULL);
+		if (client_sock == INVALID_SOCKET)
+		{
+			printf("accept()");
+			break;
+		}
+		SetEvent(hWriteEvent);
+	}
+
+	// 윈속 종료
+	WSACleanup();
+
+	return 0;
+}
+
+// 비동기 입출력 시작 함수
+DWORD __stdcall WorkerThread(void* arg)
+{
+	int retval;
+
+	while (1)
+	{
+		while (1)
+		{
+			// alertable wait
+			DWORD result = WaitForSingleObjectEx(hWriteEvent, INFINITE, TRUE);
+			if (result == WAIT_OBJECT_0) break;
+			if (result != WAIT_IO_COMPLETION) return 1;
+		}
+
+		// 접속한 클라이언트 정보 출력
+		SOCKADDR_IN clientaddr;
+
+		// 소켓 정보 구조체 할당과 초기화
+		SOCKETINFO* ptr = new SOCKETINFO;
+		if (ptr == NULL)
+			return 1;
+
+		ZeroMemory(&ptr->overlapped, sizeof(ptr->overlapped));
+		ptr->sock = client_sock;
+	}
+}
